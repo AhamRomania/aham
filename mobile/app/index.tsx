1c6bfd7b3928659a30ca2dc60702bd965fc121f0
Cosmin Albulescu <cosmin.albulescu@gmail.com>
2025-01-11 19:26:21 +0200

    Use Expo for mobile

---
+++ mobile/app/index.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#286fb4",
+      }}
+    >
+      <Stack.Screen options={{title:"Aham"}} />
+      <Text style={style.text}>Hello World!</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  text: {
+    color: "white",
+  }
+});
