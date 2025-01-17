152c8e9f17756142d831e0414f2393ca565fa004
Cosmin Albulescu <cosmin.albulescu@devias.io>
2025-01-17 19:02:17 +0200

    Cdn upload/serve routes

--- backend/service/cdn/main.go
+++ backend/service/cdn/main.go
@@ -1,18 +1,65 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 )
+
+var redisc *redis.Client
+
+func init() {
+
+	opts, err := redis.ParseURL(os.Getenv("REDIS"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to redis @", opts.Addr)
+
+	redisc = redis.NewClient(opts)
+}
 
 func main() {
 
 	mux := chi.NewMux()
 
-	mux.Get("/cdn/*", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("CDN"))
-	})
+	mux.Post("/", upload)
 
-	http.ListenAndServe(":8080", mux)
+	mux.Get("/{uuid}", serve)
+
+	listen := os.Getenv("LISTEN")
+
+	fmt.Println("Server is listening on", listen)
+
+	if err := http.ListenAndServe(listen, mux); err != nil {
+		fmt.Println("Error starting server", err)
+	}
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+
+	cmd := redisc.Get(
+		context.Background(),
+		chi.URLParam(r, "uuid"),
+	)
+
+	if cmd.Err() != nil {
+		http.Error(w, "Nu am găsit resursa căutată. Eroare 404.", http.StatusNotFound)
+		return
+	}
+
+	w.Write([]byte(cmd.Val()))
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	rid := uuid.New().String()
+	redisc.Set(context.Background(), rid, "hello", 0)
+	w.Write([]byte(`{"uuid":"` + rid + `"}`))
 }
