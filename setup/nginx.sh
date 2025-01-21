3c44b5ef9b0eba0e653d5c7ba2e00a5747aa6404
Cosmin Albulescu <cosmin.albulescu@gmail.com>
2025-01-22 00:02:51 +0200

    ci:fix

--- setup/nginx.sh
+++ setup/nginx.sh
@@ -11,11 +11,14 @@ exit
 fi
 
 rm -rf /var/www/aham.ro
+rm -f /etc/nginx/sites-enabled/aham.conf
 
 mkdir -p /var/www/aham.ro/api
 mkdir -p /var/www/aham.ro/cdn
 mkdir -p /var/www/aham.ro/web
 mkdir -p /var/www/aham.ro/certs
+
+chmod 775 /var/www/aham.ro
 
 cp -f conf/acme-aham.conf /etc/nginx/sites-enabled/acme-aham.conf
 
@@ -32,6 +35,10 @@ chown -R www-data:www-data /var/www/aham.ro
 rm -f /etc/nginx/sites-enabled/acme-aham.conf
 
 rm -rf /var/www/aham.ro/certs
+
+# aham/eeeee
+
+echo 'aham:$apr1$gc45tm9p$fo6DJ1rh5c4XHfMw1IU7h0' > /var/www/aham.ro/.htpasswd
 
 cp -f conf/aham.conf /etc/nginx/sites-enabled/aham.conf
 
