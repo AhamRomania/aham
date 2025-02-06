62310b89027ee745da6c8e0f26ccbc7cebf8f5a6
Cosmin Albulescu <cosmin.albulescu@gmail.com>
2025-02-06 02:21:23 +0200

    Fix title, rename funcs

--- wp/theme/aham/functions.php
+++ wp/theme/aham/functions.php
@@ -1,9 +1,15 @@
 <?php
 
-function mytheme_enqueue_styles() {
-    wp_enqueue_style('mytheme-style', get_stylesheet_uri());
+function aham_enqueue_styles() {
+    wp_enqueue_style('aham-style', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'aham_enqueue_styles');
 
 add_theme_support( 'post-thumbnails' );
+
+function aham_wp_title( $title, $sep ) {
+    return $title . 'Aham';
+}
+
+add_filter( 'wp_title', 'aham_wp_title', 10, 2 );
