82b96e02dbe7944df8dabffa7778e95746290f56
Cosmin Albulescu <cosmin.albulescu@gmail.com>
2025-02-06 02:32:09 +0200

    Fix bug

--- backend/service/api/vo/user.go
+++ backend/service/api/vo/user.go
@@ -5,6 +5,7 @@ import "aham/service/api/db"
 type User struct {
 	ID         int64   `json:"id"`
 	Email      string  `json:"email,omitempty"`
+	Role       string  `json:"role,omitempty"`
 	GivenName  string  `json:"given_name,omitempty"`
 	FamilyName string  `json:"family_name,omitempty"`
 	Picture    *string `json:"picture,omitempty"`
@@ -12,8 +13,8 @@ type User struct {
 
 func NewUser(u *db.User) User {
 	return User{
-		ID: u.ID,
-		//Email:      u.Email,
+		ID:         u.ID,
+		Role:       u.Role,
 		GivenName:  u.GivenName,
 		FamilyName: u.FamilyName,
 		Picture:    u.Picture,
