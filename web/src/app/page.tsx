31d27f94fd865fdac62b68e2c6896d3e242d35ba
Cosmin Albulescu <cosmin.albulescu@devias.io>
2025-02-19 23:15:05 +0200

    Fix

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import getApiFetch from "@/api/api";
+import { fetchCategory } from "@/api/categories";
 import { CategoryListSection } from "@/c/Categories";
 import { seo } from "@/c/funcs";
 import { HomepageLayout } from "@/c/Layout";
@@ -24,8 +25,8 @@ export default async function Home () {
   const recommended = await api<Ad[]>('/ads?mode=recommended');
   return (
     <HomepageLayout>
-      <CategoryListSection id={1} count={20} />
-      <CategoryListSection id={3} count={20} />
+      <CategoryListSection category={await fetchCategory(1)} />
+      <CategoryListSection category={await fetchCategory(2)} />
       <MoreAds title="Promovate" ads={promotion}/>
       <MoreAds title="Recomandate" ads={recommended}/>
     </HomepageLayout>
