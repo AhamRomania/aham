244a692e7fa0d180d4e10255cbae23cfb88e2293
Cosmin Albulescu <cosmin.albulescu@devias.io>
2025-02-24 04:36:26 +0200

    remove shares, and fix balance display

--- setup/db/api/_4_shares.sql
+++
@@ -1,8 +0,0 @@
-create table shares (
-    id serial primary key,
-    ad_id integer not null references ads(id),
-    description text,
-    platforms jsonb not null,
-    created_at timestamp not null default now(),
-    updated_at timestamp not null default now()
-)
--- web/src/c/Layout/account.tsx
+++ web/src/c/Layout/account.tsx
@@ -377,7 +377,7 @@ const AccountLayout = ({ children }: React.PropsWithChildren) => {
                   }
                 `}
               >
-                <strong>{balance > 0 ? toMoney(balance) : 100}</strong>
+                <strong>{balance > 0 ? toMoney(balance) : 0}</strong>
                 <Image alt="coins" width={16} height={16} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABVUlEQVR4nO1WQU7DMBA0ildQfoA48RDu5RnlSOkX+AS8Agl2Qx/Asb3xBFQOIBDXquz2YLSRGy7QJBtHlVBGsuTE6804Ox7buR5GhEd3sL7PzhjhhhHmQv6ZEZbaYn+uY+s8G2qsSwkmOGeCdyEIdZrGMsEoyceF/PgnuX8S8leC/nSV75+EB3eoTfv6rhgrYsr4cXsC6BdFMvSTENxeVbzGSO4v45xFewIUV2MhQBDSESBLCRIS4IYiTE4g6DbMsyEjXDPC7JdtONOxzTZMXwL8rxoIfzvcihHe6tbdpAFu6HAx+WdRb4IPRvgya0A6crjaGhCLw6GfRAIvy+ngONy6gVkDYlF3SSCBBqSlupngNQrVqgHoxOFqH0bS0uHCnTvS561/Cv3FzjTAVRcS6VgDVQty/SknbYwozVXL2614m7qbgAlGO7tub9DUB8qJPXo4G74BQpgHCXHbt1AAAAAASUVORK5CYII=" />
               </div>
               <div
