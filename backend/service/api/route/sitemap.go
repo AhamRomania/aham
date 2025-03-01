209c1da969458f3ae9f878111e6b64656a3ae45b
Cosmin Albulescu <cosmin.albulescu@devias.io>
2025-03-01 08:53:11 +0200

    fix

--- backend/service/api/route/sitemap.go
+++ backend/service/api/route/sitemap.go
@@ -4,6 +4,7 @@ import (
 	"aham/common/c"
 	"aham/service/api/db"
 	"bytes"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -14,18 +15,18 @@ func Sitemap(res http.ResponseWriter, req *http.Request) {
 	b.WriteString(`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`)
 
 	for _, c := range db.GetCategoriesFlat() {
-		writeLoc(b, c.Link(), time.Now().Format("2006-01-02"))
+		writeLoc(b, c.Link(), time.Now().Format("2006-01-02"), 0.3)
 	}
 
 	for _, ad := range db.GetAds(0, db.Filter{}) {
-		writeLoc(b, c.URLF(c.Web, ad.Href), ad.Created.Format("2006-01-02"))
+		writeLoc(b, c.URLF(c.Web, ad.Href), ad.Created.Format("2006-01-02"), 0.7)
 	}
 
 	b.WriteString(`</urlset>`)
 	res.Write(b.Bytes())
 }
 
-func writeLoc(b *bytes.Buffer, loc, lastmod string) {
+func writeLoc(b *bytes.Buffer, loc, lastmod string, priority float32) {
 	b.WriteString(`<url>`)
 	b.WriteString(`<loc>`)
 	b.WriteString(loc)
@@ -33,5 +34,6 @@ func writeLoc(b *bytes.Buffer, loc, lastmod string) {
 	b.WriteString(`<lastmod>`)
 	b.WriteString(lastmod)
 	b.WriteString(`</lastmod>`)
+	b.WriteString(fmt.Sprintf(`<changefreq>%.1f</changefreq>`, priority))
 	b.WriteString(`</url>`)
 }
